c56257c67edee692d6a8ec6c195034a1cd51223c
puppet <puppet@epochcapital.com.au>
2015-08-13 15:33:31 +1000

    nginx

--- test/modules/hosts/manifests/init.pp
+++ test/modules/hosts/manifests/init.pp
@@ -1,4 +1,4 @@
-class host {
+class hosts {
 	host { 'puppettest.epochcapital.com.au':
 		ensure  => present,
 		ip 	=> '127.0.0.1',
---
+++ test/modules/nginx/manifests/init.pp
@@ -0,0 +1,20 @@
+class nginx {
+	package { 'nginx':
+		ensure => present,
+	}
+
+	file { '/var/www':
+		ensure 	=> directory,
+		owner 	=> root,
+		group	=> root,
+		mode	=>'0755',
+	}	
+	
+	file { '/var/www/index.html':
+		ensure 	=> file,
+		owner	=> root,
+		group	=> root,
+		mode	=> '0664'
+		source	=> 'puppet:///modules/nginx/index.html',
+	}
+}
