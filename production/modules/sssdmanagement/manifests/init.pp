7b3225b05608eb9b11c37bcc897a9b0c086c1bb5
puppet <puppet@epochcapital.com.au>
2015-09-23 15:09:10 +1000

    branch

--- production/modules/sssdmanagement/manifests/init.pp
+++
@@ -1,4 +0,0 @@
-class sssdmanagement {
-  class {'::sssd':
-	}
-}
---
+++ test/modules/base/snmp.pp
@@ -0,0 +1,6 @@
+class base::snmp {
+	class { '::snmp':
+		agentaddress => ['udp:161'],
+                ro_community => hiera('snmp:community'),
+        }
+}
