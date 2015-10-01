a50beb6b6dcc992c48ec24f8bd94a76193eb4ccc
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2015-10-01 11:27:13 +1000

    repo

--- test/modules/base/manifests/packages.pp
+++ test/modules/base/manifests/packages.pp
@@ -1,4 +1,8 @@
 class base::packages {
+  file {'/etc/zypp/repo.d/SUSE_Linux_Enterprise_Server_11_SP3.repo'
+    ensure => present,
+    content => template 'sles/11sp3/sles11sp3dvd1'
+}
   $packages = hiera("packages")
   $packages.each |String $packages|{
   	package { "$packages":
