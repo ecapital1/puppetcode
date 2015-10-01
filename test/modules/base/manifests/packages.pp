2a69d28c06455e7c6d843450be7c763938bc842e
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2015-10-01 15:03:29 +1000

    newrepofix

--- test/modules/base/manifests/packages.pp
+++ test/modules/base/manifests/packages.pp
@@ -3,12 +3,14 @@ class base::packages {
   $repofiles = [ "$repodir/SLES11SP3DVD1.repo","$repodir/SLES11SP3DVD2.repo" ]
   $sourcedir = "puppet:///modules/base/sles/11sp3/"
   $sourcefile = [ "sles11sp3dvd1","sles11sp3dvd2"]
+  $repofiles.each |String $repofiles|{
   file { $repofiles:
     ensure => "present",
     owner => "root",
     group => "root",
     mode => 0644,
     source => "$sourcedir/$sourcefile"
+  }
 }
   $packages = hiera("packages")
   $packages.each |String $packages|{
