b5c76fc3c0005f1b2f4c3ed08be0eb15f0b13937
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2015-10-01 13:13:59 +1000

    reposfix5

--- test/modules/base/manifests/packages.pp
+++ test/modules/base/manifests/packages.pp
@@ -1,5 +1,5 @@
 class base::packages {
-  file {'/etc/zypp/repo.d/SUSE_Linux_Enterprise_Server_11_SP3.repo':
+  file {'/etc/zypp/repos.d/SUSE_Linux_Enterprise_Server_11_SP3.repo':
     ensure => file,
     source => 'puppet:///modules/base/sles/11sp3/sles11sp3dvd1'
 }
