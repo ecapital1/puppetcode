c13eee4dff66be53c813925c8411536b16214567
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2015-10-02 09:17:18 +1000

    new repos

--- test/modules/base/manifests/repos.pp
+++ test/modules/base/manifests/repos.pp
@@ -1,20 +1,48 @@
 class base::repos {
-	zypprepo { 'SLES11SP3DVD1':
-		baseurl		=> 'ftp://10.10.10.81/SLES-11-SP3/DVD1',
-		enabled		=> 1,
-		autorefresh	=> 1,
-		name		=> 'SLES11SP3DVD1',
-		path		=> '/',
-		type		=> yast2,
-		keeppackages	=> 0,
+	case $::osfamily {
+	'Suse': {
+		case $::operatingsystemrelease {
+			'11.3': {
+					zypprepo { 'SLES11SP3DVD1':
+						baseurl					=> 'ftp://10.10.10.81/SLES-11-SP3/DVD1',
+						enabled					=> 1,
+						autorefresh			=> 1,
+						name						=> 'SLES11SP3DVD1',
+						path						=> '/',
+						type						=> yast2,
+						keeppackages		=> 0,
+					}
+					zypprepo { 'SLES11SP3DVD2':
+    				baseurl         => 'ftp://10.10.10.81/SLES-11-SP3/DVD2',
+    				enabled         => 1,
+    				autorefresh     => 1,
+    				name            => 'SLES11SP3DVD2',
+    				path            => '/',
+    				type            => yast2,
+    				keeppackages    => 0,
+					}
+				}
+			'12.0': {
+						zypprepo { 'SLES12DVD1':
+							baseurl					=> 'ftp://10.10.10.81/SLES-12/DVD1',
+							enabled					=> 1,
+							autorefresh			=> 1,
+							name						=> 'SLES12DVD1',
+							path						=> '/',
+							type						=> yast2,
+							keeppackages		=> 0,
+						}
+						zypprepo { 'SLES12DVD2':
+							baseurl         => 'ftp://10.10.10.81/SLES-12/DVD2',
+							enabled         => 1,
+							autorefresh     => 1,
+							name            => 'SLES12DVD2',
+							path            => '/',
+							type            => yast2,
+							keeppackages    => 0,
+						}
+					}
+			}
+		}
 	}
-	zypprepo { 'SLES11SP3DVD2':
-                baseurl         => 'ftp://10.10.10.81/SLES-11-SP3/DVD2',
-                enabled         => 1,
-                autorefresh     => 1,
-                name            => 'SLES11SP3DVD2',
-                path            => '/',
-                type            => yast2,
-                keeppackages    => 0,
-        }
 }
