7ac2cb427b875a880ca699fce2d8f2f6e49b00b0
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2015-10-02 12:10:16 +1000

    new repos

--- test/modules/base/manifests/packages.pp
+++ test/modules/base/manifests/packages.pp
@@ -1,20 +1,4 @@
 class base::packages {
-  $repodir = "/etc/zypp/repos.d"
-  $repofiles = [ "$repodir/SLES11SP3DVD1.repo","$repodir/SLES11SP3DVD2.repo" ]
-  $sourcedir = "puppet:///modules/base/sles/11sp3"
-  $sourcefile = [ "sles11sp3dvd1","sles11sp3dvd2" ]
-  $repofiles.each |String $repofiles|{
-    #$sourcefile.each |String $sourcefile| {
-      file { $repofiles:
-        ensure => "file",
-        owner => "root",
-        group => "root",
-        mode => '0644',
-        #source => "$sourcedir/$sourcefile"
-    #$sourcefile.each |String $sourcefile| {"$sourcedir/$sourcefile"}
-    #  }
-    }
-  }
   $packages = hiera("packages")
   $packages.each |String $packages|{
   	package { "$packages":
