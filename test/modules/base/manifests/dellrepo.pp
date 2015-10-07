6d8c44f90c87efe1f3f6634a47dd78b00db54c48
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2015-10-07 12:27:35 +1100

    test dellrepo4

--- test/modules/base/manifests/dellrepo.pp
+++ test/modules/base/manifests/dellrepo.pp
@@ -1,6 +1,10 @@
 class base::dellrepo {
   exec { "dell_repo":
-      command => "wget -q -O - http://linux.dell.com/repo/hardware/latest/bootstrap.cgi",
+      command => "wget -q -O - http://linux.dell.com/repo/hardware/latest/bootstrap.cgi > bootstrap.cgi",
       path    => "/usr/bin/",
   }
+  exec { "dell_repo_install":
+      command => "bash /root/bootstrap.cgi",
+      path    => "/bin/"
+    }
 }
