66320709f44a2a4784f98a390e93d79f23ec0bfd
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2015-10-29 17:18:49 +1100

    change to hiera packages

--- test/modules/base/manifests/dellrepo.pp
+++ test/modules/base/manifests/dellrepo.pp
@@ -1,10 +1,8 @@
 class base::dellrepo {
   exec { "dell_repo":
       command => "/usr/bin/wget -q -O - http://linux.dell.com/repo/hardware/latest/bootstrap.cgi > bootstrap.cgi",
-      #path    => "/usr/bin/",
   }
   exec { "dell_repo_install":
       command => "/bin/bash /root/bootstrap.cgi",
-      #path    => "/bin/"
     }
 }
