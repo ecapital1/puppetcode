23b1c728904156b21da428a640d34a9f85035348
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2015-10-29 17:33:27 +1100

    change to hiera packages

--- test/modules/base/manifests/init.pp
+++ test/modules/base/manifests/init.pp
@@ -1,9 +1,8 @@
 class base {
-	#include base::repos
-	#include base::dellrepo
-	#include base::python
-	#include base::snmp
-	#include base::ntp
+	include base::repos
+	include base::dellrepo
+	include base::python
+	include base::snmp
+	include base::ntp
 	include base::packages
-
 }
