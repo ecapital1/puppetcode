20ac1ecc8ece73d1a15d40bd3de4c71810371e58
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-05 16:22:44 +1100

    testing ntp

--- test/modules/base/manifests/repos.pp
+++ test/modules/base/manifests/repos.pp
@@ -2,53 +2,6 @@ class base::repos {
 	case $::osfamily {
 	'Suse': {
 		case $::operatingsystemrelease {
-			'12.0': {
-					zypprepo { 'SLES11SP3DVD1':
-						baseurl					=> 'ftp://10.10.10.81/SLES-11-SP3/DVD1',
-						enabled					=> 1,
-						autorefresh			=> 1,
-						name						=> 'SLES11SP3DVD1',
-						path						=> '/',
-						type						=> yast2,
-						keeppackages		=> 0,
-					}
-					zypprepo { 'SLES11SP3DVD2':
-    				baseurl         => 'ftp://10.10.10.81/SLES-11-SP3/DVD2',
-    				enabled         => 1,
-    				autorefresh     => 1,
-    				name            => 'SLES11SP3DVD2',
-    				path            => '/',
-    				type            => yast2,
-    				keeppackages    => 0,
-					}
-					zypprepo { 'SLE11SP3SDKDVD1':
-						baseurl					=> 'ftp://10.10.10.81/SLE-11-SP3-SDK/DVD1',
-						enabled					=> 1,
-						autorefresh			=> 1,
-						name						=> 'SLE11SP3SDKDVD1',
-						path						=> '/',
-						type						=> yast2,
-						keeppackages		=> 0,
-					}
-					zypprepo { 'SLES11SP3SDKDVD2':
-    				baseurl         => 'ftp://10.10.10.81/SLE-11-SP3-SDK/DVD2',
-    				enabled         => 1,
-    				autorefresh     => 1,
-    				name            => 'SLE11SP3SDKDVD2',
-    				path            => '/',
-    				type            => yast2,
-    				keeppackages    => 0,
-					}
-					zypprepo { 'OPENSUSE':
-    				baseurl         => 'http://download.opensuse.org/distribution/13.2/repo/oss/',
-    				enabled         => 1,
-    				autorefresh     => 1,
-    				name            => 'OPENSUSE',
-    				path            => '/',
-    				type            => yast2,
-    				keeppackages    => 0,
-					}
-				}
 			'11.3': {
 						zypprepo { 'SLES12DVD1':
 							baseurl					=> 'ftp://10.10.10.81/SLES-12/DVD1',
