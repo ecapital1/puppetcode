837bfc8fa92a72550d7542f78ed02058ba5f3548
puppet <puppet@epochcapital.com.au>
2015-11-06 15:02:34 +1100

    test

--- test/modules/base/manifests/init.pp
+++ test/modules/base/manifests/init.pp
@@ -2,11 +2,7 @@ class base {
 	include base::repos
 	#include base::dellrepo
 	#include base::python
-<<<<<<< HEAD
-	#include base::snmp
-=======
 	include base::snmp
->>>>>>> b74e1585511f83ce4bf6c422716f6dd5d2d8fa26
 	include base::ntp
 	#include base::packages
 	include base::sysctl
