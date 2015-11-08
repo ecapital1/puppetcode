4573a872f065170e1858446c84a6522a68050360
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-09 10:36:26 +1100

    updated sudoers file

--- test/modules/base/manifests/sudoers.pp
+++ test/modules/base/manifests/sudoers.pp
@@ -10,8 +10,36 @@ class base::sudoers {
     priority => 2,
     content  => "%appsupport ALL = (ALL) NOPASSWD:/bin/su  - epoch,/bin/su - postgres",
   }
-  sudo::conf { 'developer':
+  sudo::conf { 'epoch':
     priority => 3,
+    content  => "epoch ALL = (ALL) NOPASSWD:/opt/epoch/bin/tshark -i em1,/opt/epoch/bin/tcpdump ,/usr/bin/pkill tcpdump,/usr/bin/cset"
+  }
+  sudo::conf { 'developer':
+    priority => 4,
     content  => "%developer ALL = (ALL) NOPASSWD:/bin/su - epoch"
+  }
+  sudo::conf { 'MFT':
+    priority => 5,
+    content  => "%MFT ALL = (ALL) NOPASSWD:/bin/su - mft"
+  }
+  sudo::conf { 'er':
+    priority => 6,
+    content  => "%er ALL = (ALL) NOPASSWD:/bin/su - er"
+  }
+  sudo::conf { 'rdb':
+    priority => 7,
+    content  => "%rdb ALL = (ALL) NOPASSWD:/bin/su - rdb"
+  }
+  sudo::conf { 'fcs':
+    priority => 8,
+    content  => "%fcs ALL = (ALL) NOPASSWD:/bin/su - fcs"
+  }
+  sudo::conf { 'fst':
+    priority => 9,
+    content  => "%fst ALL = (ALL) NOPASSWD:/bin/su - fst"
+  }
+  sudo::conf { 'epcfg':
+    priority => 10,
+    content  => "%epcfg ALL = (ALL) NOPASSWD:/bin/su - epcfg"
   }
 }
