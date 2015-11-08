f78c173912671fc5aaee15bba1b55e64971247fa
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-09 10:18:17 +1100

    sudoers file

--- test/modules/base/manifests/sudoers.pp
+++ test/modules/base/manifests/sudoers.pp
@@ -2,8 +2,16 @@ class base::sudoers {
   class { '::sudo':
     config_file_replace => false,
   }
-  sudo::conf { 'admins':
-    priority => 10,
-    content  => "%admins ALL=(ALL) NOPASSWD: ALL",
+  sudo::conf { 'ITAdmin':
+    priority => 1,
+    content  => "%ITAdmin ALL = (ALL) NOPASSWD:/bin/su -"
+  }
+  sudo::conf { 'appsupport':
+    priority => 2,
+    content  => "%appsupport ALL = (ALL) NOPASSWD:/bin/su  - epoch,/bin/su - postgres",
+  }
+  sudo::conf { 'developer':
+    priority => 3,
+    content  => "%developer ALL = (ALL) NOPASSWD:/bin/su - epoch"
   }
 }
