5e89fe8027e71a22af93fbeb03db1909ad84b1c1
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-09 08:46:20 +1100

    sudoers file

---
+++ test/modules/base/manifests/sudoers.pp
@@ -0,0 +1,9 @@
+class base::sudoers {
+  class { ::'sudo':
+    config_file_replace => false,
+    sudo::conf { 'admins':
+      priority => 10,
+      content  => "%admins ALL=(ALL) NOPASSWD: ALL",
+    }
+  }
+}
