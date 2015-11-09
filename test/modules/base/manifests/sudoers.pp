d213c0d769cd204d6c865994234dec434a0ca663
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-09 15:08:45 +1100

    test new fact

--- test/modules/base/manifests/sudoers.pp
+++ test/modules/base/manifests/sudoers.pp
@@ -1,14 +1,51 @@
 class base::sudoers {
-  case $::role {
-    'mft': {
-      file { '/etc/mft':
-        ensure => file,
-        mode => '0644',
-      }
-    }
-  }
   class { '::sudo':
     config_file_replace => false,
+  }
+  case $::role {
+    'mft': {
+      sudo::conf { 'MFT':
+        priority => 5,
+        content  => "%MFT ALL = (ALL) NOPASSWD:/bin/su - mft"
+        }
+      }
+    'er': {
+      sudo::conf { 'er':
+        priority => 6,
+        content  => "%er ALL = (ALL) NOPASSWD:/bin/su - er"
+        }
+      }
+    'developer': {
+      sudo::conf { 'developer':
+        priority => 4,
+        content  => "%developer ALL = (ALL) NOPASSWD:/bin/su - epoch"
+      }
+    }
+    'rdb': {
+      sudo::conf { 'rdb':
+        priority => 7,
+        content  => "%rdb ALL = (ALL) NOPASSWD:/bin/su - rdb"
+      }
+    }
+    'fcs': {
+      sudo::conf { 'fcs':
+        priority => 8,
+        content  => "%fcs ALL = (ALL) NOPASSWD:/bin/su - fcs"
+      }
+    }
+    'fst': {
+      sudo::conf { 'fst':
+        priority => 9,
+        content  => "%fst ALL = (ALL) NOPASSWD:/bin/su - fst"
+      }
+    }
+    'epcfg': {
+      sudo::conf { 'epcfg':
+        priority => 10,
+        content  => "%epcfg ALL = (ALL) NOPASSWD:/bin/su - epcfg"
+      }
+    }
+    }
   }
   sudo::conf { 'ITAdmin':
     priority => 1,
@@ -22,32 +59,5 @@ class base::sudoers {
     priority => 3,
     content  => "epoch ALL = (ALL) NOPASSWD:/opt/epoch/bin/tshark -i em1,/opt/epoch/bin/tcpdump ,/usr/bin/pkill tcpdump,/usr/bin/cset"
   }
-  sudo::conf { 'developer':
-    priority => 4,
-    content  => "%developer ALL = (ALL) NOPASSWD:/bin/su - epoch"
-  }
-  sudo::conf { 'MFT':
-    priority => 5,
-    content  => "%MFT ALL = (ALL) NOPASSWD:/bin/su - mft"
-  }
-  sudo::conf { 'er':
-    priority => 6,
-    content  => "%er ALL = (ALL) NOPASSWD:/bin/su - er"
-  }
-  sudo::conf { 'rdb':
-    priority => 7,
-    content  => "%rdb ALL = (ALL) NOPASSWD:/bin/su - rdb"
-  }
-  sudo::conf { 'fcs':
-    priority => 8,
-    content  => "%fcs ALL = (ALL) NOPASSWD:/bin/su - fcs"
-  }
-  sudo::conf { 'fst':
-    priority => 9,
-    content  => "%fst ALL = (ALL) NOPASSWD:/bin/su - fst"
-  }
-  sudo::conf { 'epcfg':
-    priority => 10,
-    content  => "%epcfg ALL = (ALL) NOPASSWD:/bin/su - epcfg"
-  }
+
 }
