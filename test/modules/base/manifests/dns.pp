24819803c7b029cbc0c1ee601f9d9903bc43ed5d
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-09 15:25:36 +1100

    new dns pp

--- test/modules/base/manifests/dns.pp
+++ test/modules/base/manifests/dns.pp
@@ -1,6 +1,6 @@
 class base::dns {
   class { '::dnsclient':
-    nameservers => '10.10.10.2, 10.129.1.21'
-    domain      => 'epochcapital.com.au'
+    nameservers => '10.10.10.2, 10.129.1.21',
+    domain      => 'epochcapital.com.au',
   }
 }
