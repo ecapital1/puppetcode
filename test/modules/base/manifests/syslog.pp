17abdea4608b8685ff5a72e20d15128aa795bb55
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-10 11:59:12 +1100

    new syslog pp

--- test/modules/base/manifests/syslog.pp
+++ test/modules/base/manifests/syslog.pp
@@ -1,6 +1,6 @@
 class base::syslog {
   class { '::rsyslog::client':
-      servers => '10.10.10.5',
+      server => '10.10.10.5',
       port    => '514',
     #  pattern => 'local2.*'
       custom_config => 'base/rsyslog.erb'
