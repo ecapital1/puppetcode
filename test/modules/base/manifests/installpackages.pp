30f500047c8a75dcf0eea1a357bfe3577b407c71
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-11 14:37:23 +1100

    update to test env

--- test/modules/base/manifests/installpackages.pp
+++ test/modules/base/manifests/installpackages.pp
@@ -1,8 +1,8 @@
 class base::installpackages {
     $packagelist = hiera('ospackages')
-    $packagelist.each |String $packagelist1a|{
-      package { '$packagelist1':
-        ensure => installed,
-    }
+    $packagelist.each |String $packagelist|{
+    #  package { '$packagelist1':
+    #    ensure => installed,
+    #}
   }
 }
