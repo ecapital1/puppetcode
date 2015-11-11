e2cbe18f4f3d2c50411e83151d7231b6fd4fe9ad
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-11 17:32:43 +1100

    sssd install

--- test/modules/base/manifests/init.pp
+++ test/modules/base/manifests/init.pp
@@ -4,10 +4,11 @@ class base {
 	#include base::python
 	include base::snmp
 	include base::ntp
-	include base::installpackages
+	#include base::installpackages
 	include base::sysctl
 	include base::sudoers
 	include base::dns
 	include base::syslog
 	include base::ssh
+	include base::sssd
 }
