d52d7f6f4e6727f5e648490acc35b00751544fa1
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-11 18:04:19 +1100

    sssd config

--- test/modules/base/manifests/sssd.pp
+++ test/modules/base/manifests/sssd.pp
@@ -2,11 +2,26 @@ class base::sssd {
   class { '::sssd':
     config => {
       'sssd' => {
-        'key1' => 'value1',
-        'keyX' => [ 'valueY', 'valueZ' ],
+        'config_file_version' => '2',
+        'services' => [ 'nss', 'pam' ],
+        'domains'  => 'LDAP',
         },
       'domain/LDAP' => {
-        'key2' => 'value2',
+        'ldap_uri'                => 'ldap://10.10.10.147',
+        'ldap_search_base'        => 'dc=epochcapital,dc=com,dc=au',
+        'ldap_schema'             => 'rfc2307',
+        'id_provider'             => 'ldap',
+        'ldap_user_uuid'          => 'entryuuid',
+        'ldap_group_uuid'         => 'entryuuid',
+        'ldap_tls_reqcert'        => 'never',
+        'ldap_id_use_start_tls'   => 'False',
+        'enumerate'               => 'True',
+        'cache_credentials'       => 'True',
+        'chpass_provider'         => 'ldap',
+        'auth_provider'           => 'ldap',
+        'access_provider'         => 'simple',
+        'simple_allow_groups'     => 'ITAdmin, appsupport, developer',
+        'simple_allow_users'      => 'epoch, appsupport',
         },
       }
     }
