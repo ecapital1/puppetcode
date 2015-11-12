6f38233911544680f037623fc7a6a708379d6d3c
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-13 10:53:25 +1100

    fix for installpackages

--- test/modules/base/manifests/installpackages.pp
+++ test/modules/base/manifests/installpackages.pp
@@ -1,6 +1,6 @@
 class base::installpackages {
-    notify {"install package running on $::osfamily and on $::operatingsystemrelease"}
-    $packages = hiera_array('ospackages')
+    notify {"install package running on $::osfamily and on $::operatingsystemrelease"},
+    $packages = hiera_array('ospackages'),
     $packages.each |String $packages|{
       package { $packages:
         ensure => installed,
