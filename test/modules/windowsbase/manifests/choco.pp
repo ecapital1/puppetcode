5c2f3ce24c1fc3516c8f6b336c40b31623ad0f0f
Patrick Roughan <padraig.oruchain@gmail.com>
2015-11-13 16:25:50 +1100

    rename windows folder

--- test/modules/windowsbase/manifests/choco.pp
+++ test/modules/windowsbase/manifests/choco.pp
@@ -1,10 +1,7 @@
 class windows::choco {
   class {'::chocolatey':}
 }
-case $operatingsystem {
-  'windows':
-    Package { provider => chocolatey, }
-}
+Package { provider => chocolatey, }
 package { 'notepadplusplus':
   ensure   => latest,
   provider => 'chocolatey',
