7161309912392be1549b92686b8f99ddf3872554
Patrick Roughan <padraig.oruchain@gmail.com>
2015-12-10 17:29:36 +1100

    add users

--- test/modules/base/manifests/users.pp
+++ test/modules/base/manifests/users.pp
@@ -1,9 +1,7 @@
 class base::users {
-#    accounts::user { 'epoch':
-#    }
-#}
-::accounts::user { 'epoch':
-  shell    => '/bin/bash',
-  comment  => 'epoch account',
-}
+  class { '::accounts':
+      account => {'epoch':
+        shell    => '/bin/bash',
+      }
+  }
 }
