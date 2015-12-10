39702f831d13897288e952c1571f880ee89b4c08
Patrick Roughan <padraig.oruchain@gmail.com>
2015-12-10 17:18:50 +1100

    add users

--- test/modules/base/manifests/users.pp
+++ test/modules/base/manifests/users.pp
@@ -1,4 +1,20 @@
-class base::users {
-    accounts::user { 'epoch':
-    }
+#class base::users {
+#    accounts::user { 'epoch':
+#    }
+#}
+accounts::user { 'jeff':
+  shell    => '/bin/zsh',
+  comment  => 'Jeff McCune',
+  groups   => [
+    'admin',
+    'sudonopw',
+  ],
+  uid      => 1112,
+  gid      => 1112,
+  locked   => true,
+  sshkeys  => [
+    'ssh-rsa AAAA...',
+    'ssh-dss AAAA...',
+  ],
+  password => '!!',
 }
