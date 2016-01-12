d5fdd642a8497eed80cb6370d4a421d7632bccee
Patrick Roughan <padraig.oruchain@gmail.com>
2016-01-12 17:12:01 +1100

    update snmp.pp

--- test/modules/base/manifests/snmp.pp
+++ test/modules/base/manifests/snmp.pp
@@ -2,6 +2,6 @@ class base::snmp {
 	class { '::snmp':
 		agentaddress => ['udp:161'],
 		ro_community => hiera('snmp.community'),
-		ro_net			 => ('10.10.10.5'),
+		ro_network	 => ('10.10.10.5'),
         }
 }
