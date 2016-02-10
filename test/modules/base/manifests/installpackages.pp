7e5fee090ee960842d59c8f72a1ac64c1f864f5c
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-10 15:24:24 +1100

    init.pp

--- test/modules/base/manifests/installpackages.pp
+++ test/modules/base/manifests/installpackages.pp
@@ -1,6 +1,6 @@
 class base::installpackages {
-    notice ("install package running on $::osfamily and on $::operatingsystemrelease"),
-    $packages = hiera_array('ospackages'),
+    notice ("install package running on $::osfamily and on $::operatingsystemrelease");
+    $packages = hiera_array('ospackages');
     $packages.each |String $packages|{
       package { $packages:
         ensure => installed,
