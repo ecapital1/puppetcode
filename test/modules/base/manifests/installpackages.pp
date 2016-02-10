ce558631633f3b669d263c7297305020d142bf41
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-10 15:26:02 +1100

    init.pp

--- test/modules/base/manifests/installpackages.pp
+++ test/modules/base/manifests/installpackages.pp
@@ -1,12 +1,12 @@
 class base::installpackages {
-    notice ("install package running on $::osfamily and on $::operatingsystemrelease");
-    $packages = hiera_array('ospackages');
-    $packages.each |String $packages|{
-      package { $packages:
-        ensure => installed,
-      }
-    }
-    file {"/var/run/bacula/":
-      ensure => 'directory',
-    }
+    notice ("install package running on $::osfamily and on $::operatingsystemrelease")
+    $packages = hiera_array('ospackages')
+    #$packages.each |String $packages|{
+    #  package { $packages:
+    #    ensure => installed,
+    #  }
+    #}
+    #file {"/var/run/bacula/":
+    #  ensure => 'directory',
+    #}
 }
