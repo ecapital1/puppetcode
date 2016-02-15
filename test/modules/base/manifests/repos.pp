1ca72436d0d579005299356b498665f84cbeeb08
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-15 16:06:31 +1100

    add repo

--- test/modules/base/manifests/repos.pp
+++ test/modules/base/manifests/repos.pp
@@ -3,38 +3,38 @@ class base::repos {
 	'Suse': {
 		case $::operatingsystemrelease {
 			'11.3', '12.1': {
-						zypprepo { 'SLES12DVD1':
+						zypprepo { 'SLES12SP1DVD1':
 							baseurl					=> 'ftp://10.10.10.81/SLES-12-SP1/DVD1',
 							enabled					=> 1,
 							autorefresh			=> 1,
-							name						=> 'SLES12DVD1',
+							name						=> 'SLES12SP1DVD1',
 							path						=> '/',
 							type						=> yast2,
 							keeppackages		=> 0,
 						}
-						zypprepo { 'SLES12DVD2':
+						zypprepo { 'SLES12SP1DVD2':
 							baseurl         => 'ftp://10.10.10.81/SLES-12-SP1/DVD2',
 							enabled         => 1,
 							autorefresh     => 1,
-							name            => 'SLES12DVD2',
+							name            => 'SLES12SP1DVD2',
 							path            => '/',
 							type            => yast2,
 							keeppackages    => 0,
 						}
-						zypprepo { 'SLES12SDKDVD1':
+						zypprepo { 'SLES12SDKSP1DVD1':
 							baseurl					=> 'ftp://10.10.10.81/SLES-12-SDK-SP1/DVD1',
 							enabled					=> 1,
 							autorefresh			=> 1,
-							name						=> 'SLES12SDKDVD1',
+							name						=> 'SLES12SDKSP1DVD1',
 							path						=> '/',
 							type						=> yast2,
 							keeppackages		=> 0,
 						}
-						zypprepo { 'SLES12SDKDVD2':
+						zypprepo { 'SLES12SDKSP1DVD2':
 							baseurl         => 'ftp://10.10.10.81/SLES-12-SDK-SP1/DVD2',
 							enabled         => 1,
 							autorefresh     => 1,
-							name            => 'SLES12SDKDVD2',
+							name            => 'SLES12SDKSP1DVD2',
 							path            => '/',
 							type            => yast2,
 							keeppackages    => 0,
