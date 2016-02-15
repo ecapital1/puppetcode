aff8b78585449fa34f94cd45a3039787de2b58b5
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-15 13:46:35 +1100

    installpackages

--- test/modules/base/manifests/installpackages.pp
+++ test/modules/base/manifests/installpackages.pp
@@ -1,13 +1,4 @@
 class base::installpackages {
-    #notice ("install package running on $::osfamily and on $::operatingsystemrelease");
-    $packages = ['a', 'b', 'c']#hiera_array('ospackages')
-    $packages.each |String $package| {
-      notice $package
-      #package { $package:
-      #  ensure => installed,
-      #}
-    }
-#    file {"/var/run/bacula/":
-#      ensure => 'directory',
-#    }
+  $packages = hiera_array('ospackages')
+
 }
