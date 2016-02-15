e4a6f9ceba48255ec43f77a68e2c3484dd14620d
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-15 13:36:52 +1100

    init.pp

--- test/modules/base/manifests/installpackages.pp
+++ test/modules/base/manifests/installpackages.pp
@@ -1,6 +1,6 @@
 class base::installpackages {
-    notice ("install package running on $::osfamily and on $::operatingsystemrelease")
-    $packages = ['a', 'b', 'c'];#hiera_array('ospackages')
+    notice ("install package running on $::osfamily and on $::operatingsystemrelease");
+    $packages = ['a', 'b', 'c']#hiera_array('ospackages')
     notice $packages;
     #$packages.each {
     #  notice $package
