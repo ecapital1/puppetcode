3a794aa465ba143d6589489ee5df3041a94c0377
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-15 14:07:07 +1100

    installpackages

--- test/modules/base/manifests/installpackages.pp
+++ test/modules/base/manifests/installpackages.pp
@@ -1,14 +1,8 @@
 class base::installpackages {
-  #$data = ["routers", "servers", "workstations"]
-["routers", "servers", "workstations"].each |$item| {
-    notify { $item:
-      message => $item
+  $packages = hiera_array('ospackages')
+  $package = each($packages){
+    package { $package:
+      ensure => installed,
     }
-  }
-#  $packages = hiera_array('ospackages')
-#  $packages.each |$index, $packages|{
-#    package { $packages:
-#      ensure => installed,
-#    }
 #  }
 }
