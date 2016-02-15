626da84ae8c627b1a2f970bfb268b2d7bbdb2487
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-15 14:16:37 +1100

    installpackages

--- test/modules/base/manifests/installpackages.pp
+++ test/modules/base/manifests/installpackages.pp
@@ -1,3 +1,7 @@
 class base::installpackages {
   notice ("install packages running on $::osfamily and on $::operatingsystemrelease")
+  $ospackages = hiera_array('ospackages')
+  $ospackages.each |String $packages|{
+    
+  }
 }
