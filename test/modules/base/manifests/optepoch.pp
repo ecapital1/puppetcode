efdec6d154e85658bc19094a41ff510fe8c99f12
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-18 11:39:22 +1100

    add optepoch.pp

--- test/modules/base/manifests/optepoch.pp
+++ test/modules/base/manifests/optepoch.pp
@@ -1,7 +1,7 @@
 class base::optepoch {
   file { "/opt/epoch/":
     ensure  => 'directory',
-    source  => 'file:///opt/epoch/',
+    source  => 'file:///opt_epoch/',
     recurse => true,
   }
 }
