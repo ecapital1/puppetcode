c9ebc1a5522c38ecb884ba367716194343f27b9c
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-18 15:07:05 +1100

    add optepoch.pp

--- test/modules/base/manifests/optepoch.pp
+++ test/modules/base/manifests/optepoch.pp
@@ -1,5 +1,5 @@
 class base::optepoch {
-  rsync::put { "/opt/epoch/":
+  rsync::put { "$hostname:/opt/epoch/":
     user    => 'root',
     keyfile => '/root/.ssh/id_rsa',
     source  => 'puppet:///opt_epoch/',
