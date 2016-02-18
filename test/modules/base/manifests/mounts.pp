9d39b1ae66c7c541b7578df78c303226528a1595
Patrick Roughan <padraig.oruchain@gmail.com>
2016-02-18 11:00:53 +1100

    add mounts.pp

--- test/modules/base/manifests/mounts.pp
+++ test/modules/base/manifests/mounts.pp
@@ -1,9 +1,7 @@
 class base::mounts {
   class { '::fstab': }
-    file {"/Data/rdb":
-      ensure  => 'directory',
-    }
     fstab::mount { '/Data/rdb':
+      ensure  => 'directory'
       ensure  => 'mounted',
       device  => 'epcau-srv-003:/Data/rdb/',
       options => 'auto',
