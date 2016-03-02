312a7c3baf7179562e292c095c8a01bf53a101fd
Patrick Roughan <padraig.oruchain@gmail.com>
2016-03-02 13:38:44 +1100

    new init.pp

--- test/modules/base/manifests/init.pp
+++ test/modules/base/manifests/init.pp
@@ -1,24 +1,34 @@
 class base {
-	include base::repos
-	include base::dellrepo
-	#include base::python
-	include base::snmp
-	include base::ntp
-	include base::installpackages
-	include base::sysctl
-	include base::sudoers
-	include base::dns
-	include base::syslog
-	include base::ssh
-	include base::sssd
-	#include base::users
+	case $operatingsystem {
+		'SLES': {
+			include base::repos
+			include base::dellrepo
+			#include base::python
+			include base::snmp
+			include base::ntp
+			include base::installpackages
+			include base::sysctl
+			include base::sudoers
+			include base::dns
+			include base::syslog
+			include base::ssh
+			include base::sssd
+			#include base::users
+			include base::optepoch
+		}
+		'Ubuntu': {
+			include base::ntp
+		#	include base::installpackages
+			include base::sudoers
+			include base::dns
+			include base::ssh
+			include base::sssd
+		}
+	}
+
 	case $hostname {
 		'epcau-srv-research': {
 			include base::mounts
-			include base::optepoch
-		}
-		'epose-srv-003': {
-			include base::optepoch
 		}
 		default: {
 			notice("only for selected servers")
