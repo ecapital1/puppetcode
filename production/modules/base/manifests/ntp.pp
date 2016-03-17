262a756edfc7828d7c77a7486ba04f0629e7a8b0
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2016-03-18 10:47:50 +1100

    changed ntp conf

--- production/modules/base/manifests/ntp.pp
+++ production/modules/base/manifests/ntp.pp
@@ -2,7 +2,7 @@ class base::ntp {
 	class { '::ntp':
 		case $::hostname{
 			'alc-srv-011':{
-				servers => [ '10.129.1.14', '10.129.1.21' ],
+				servers => [ '10.251.25.108', '10.251.25.109' ],
 				iburst_enable => true,
 				minpoll => '4',
 				maxpoll => '4',
