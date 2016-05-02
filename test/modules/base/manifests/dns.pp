a0ce5900b0785f7a76f25e437c12e32b530ca2ca
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2016-05-02 17:40:14 +1000

    changed dns

--- test/modules/base/manifests/dns.pp
+++ test/modules/base/manifests/dns.pp
@@ -1,6 +1,6 @@
 class base::dns {
   class { '::dnsclient':
-    nameservers => [hiera('dnslocation')],
+    nameservers => hiera('dnslocation'),
     domain      => 'epochcapital.com.au',
     search      => 'epochcapital.com.au',
     options     => ['timeout:1']
