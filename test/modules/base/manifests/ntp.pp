5603a5eff6e9b61a0017ac52bc8546e86a0cc1a8
Patrick Roughan <patrick.roughan@epochcapital.com.au>
2016-05-02 11:08:40 +1000

    changed ntp

--- test/modules/base/manifests/ntp.pp
+++ test/modules/base/manifests/ntp.pp
@@ -1,8 +1,8 @@
 class base::ntp {
 	class { '::ntp':
-		servers => [ '10.129.1.14', '10.129.1.21' ],
+		servers => [ hiera('ntp') ],
 		iburst_enable => true,
 		minpoll => '4',
-		maxpoll => '4',	
+		maxpoll => '4',
 	}
 }
